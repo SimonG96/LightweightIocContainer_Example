b9d286ee999ba7d1e057abd674d65f67b282ce64
Simon G <simon.gockner@gmail.com>
2021-12-17 13:15:15 +0100

    - add validation example test

---
+++ Test.LightweightIocContainer_Example/IocValidationTest.cs
@@ -0,0 +1,24 @@
+﻿// Author: Gockner, Simon
+// Created: 2021-12-17
+// Copyright(c) 2021 SimonG. All Rights Reserved.
+
+using LightweightIocContainer;
+using LightweightIocContainer.Validation;
+using LightweightIocContainer_Example;
+using NUnit.Framework;
+
+namespace Test.LightweightIocContainer_Example;
+
+[TestFixture]
+public class IocValidationTest
+{
+    [Test]
+    public void ValidateIocContainerSetup()
+    {
+        Bootstrapper bootstrapper = new();
+        IocContainer container = bootstrapper.InstantiateContainer();
+
+        IocValidator validator = new(container);
+        validator.Validate();
+    }
+}
